22a34ee0da1116cf230fd30d9878b5475da3dca1
Sergey Petrov <sergey.petrov@cinarra.com>
2014-01-14 14:25:50 -0800

    welcome1

--- curator-pilot/src/main/java/com/cinarra/curator_pilot/coordinator/Coordinator.java
+++ curator-pilot/src/main/java/com/cinarra/curator_pilot/coordinator/Coordinator.java
@@ -17,7 +17,7 @@ public class Coordinator {
 	
 	
 	private void listen() throws Exception{
-		log.info("=============== WELCOME? ================" );
+		log.info("=============== WELCOME1 ================" );
 		
 		while(true) {
 			servers.printNodes();
