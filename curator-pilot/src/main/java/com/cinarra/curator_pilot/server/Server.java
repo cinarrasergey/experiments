d26b993d20677bbc24a2adc406aa008b69883c61
Sergey Petrov <sergey.petrov@cinarra.com>
2014-01-15 12:23:38 -0800

    minor improvements

--- curator-pilot/src/main/java/com/cinarra/curator_pilot/server/Server.java
+++ curator-pilot/src/main/java/com/cinarra/curator_pilot/server/Server.java
@@ -1,5 +1,7 @@
 package com.cinarra.curator_pilot.server;
 
+import javax.annotation.PreDestroy;
+
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.zookeeper.CreateMode;
 import org.slf4j.Logger;
@@ -7,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,12 +22,28 @@ public class Server {
 		curator.create().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath("/servers/new");
 	}
 	
-	
-	public static void main(String[] args) {
-		//another test modification
-    	ApplicationContext springContainer = new AnnotationConfigApplicationContext(ServerSpringConfig.class);
-    	Server server = springContainer.getBean(Server.class);
-    	log.info("Server started");
-    	while(true){}
+	@PreDestroy
+	public void destroy() {
+		log.warn("Destroying Server....");
 	}
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		//another test modification
+    	AbstractApplicationContext springContainer = new AnnotationConfigApplicationContext(ServerSpringConfig.class);
+    	try{
+        	Server server = springContainer.getBean(Server.class);
+        	int timeToLiveInSec=20;
+        	log.info("Server started");
+        	for (int i = timeToLiveInSec ; i >0 ; i--) {
+            	log.info("Time to live: "+i+" seconds.");
+            	Thread.sleep(1000);
+    		}
+        	log.info("Server finished");
+    	}
+    	finally{
+        	log.info("Closing Spring Context");
+    		springContainer.close();
+    	}
+ 	}
 }
