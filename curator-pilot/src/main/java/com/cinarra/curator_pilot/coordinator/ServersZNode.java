63722e667379d377c7ee831f3258a8b3b0f2ab9b
Sergey Petrov <sergey.petrov@cinarra.com>
2014-01-15 14:53:27 -0800

    adding JMX support

--- curator-pilot/src/main/java/com/cinarra/curator_pilot/coordinator/ServersZNode.java
+++ curator-pilot/src/main/java/com/cinarra/curator_pilot/coordinator/ServersZNode.java
@@ -13,16 +13,29 @@ import org.apache.curator.framework.recipes.cache.PathChildrenCache.StartMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Component;
 
 import com.google.common.io.Closeables;
 
 @Component
+@ManagedResource(
+		objectName="bean:name=serversZNode", 
+		description="Servers ZNode info")
 public class ServersZNode {
 	private static Logger log = LoggerFactory.getLogger(ServersZNode.class);
 	
 	private PathChildrenCache serversCache;
 	private CuratorFramework curator;
+	private int serverCount;
+	
+	@ManagedAttribute
+	public int getServerCount() {
+		return serverCount;
+	}
+
+
 	@Autowired
 	public ServersZNode(
 			CuratorFramework curatorFramework
@@ -36,11 +49,13 @@ public class ServersZNode {
 		curator = curatorFramework;
 	}
 	
+	
 	public void printNodes() {
 		CuratorFrameworkState state = curator.getState();
 		
 		System.out.println("\nCurrent list of Nodes As of "+new Date()+" STATE:"+state);
 		List<ChildData> currentData = serversCache.getCurrentData();
+		serverCount = currentData.size();
 		for (ChildData childData : currentData) {
 			System.out.println("  --->"+childData.getPath());
 		}
--- curator-pilot/src/main/java/com/cinarra/curator_pilot/framework/FrameworkSpringConfig.java
+++ curator-pilot/src/main/java/com/cinarra/curator_pilot/framework/FrameworkSpringConfig.java
@@ -8,6 +8,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jmx.export.MBeanExporter;
+import org.springframework.jmx.export.annotation.AnnotationJmxAttributeSource;
+import org.springframework.jmx.export.assembler.MetadataMBeanInfoAssembler;
+import org.springframework.jmx.export.naming.MetadataNamingStrategy;
 
 @Configuration
 @ComponentScan(basePackages={
@@ -16,6 +20,8 @@ import org.springframework.context.annotation.Configuration;
 public class FrameworkSpringConfig {
 	private Logger log = LoggerFactory.getLogger(FrameworkSpringConfig.class);
 
+	
+	//================== ZOOKEEPER
 	@Bean
 	public CuratorFramework curatorFramework(AppConfiguration config){
 		log.info("creating zookeeper client...");
@@ -26,5 +32,18 @@ public class FrameworkSpringConfig {
 		log.info("starting zookeeper client...");
 		client.start();
 		return client ;
+	}
+	
+	//==================== Spring JMX support
+	@Bean
+	public MBeanExporter mBeanExporter(){
+		AnnotationJmxAttributeSource attributeSource = new AnnotationJmxAttributeSource();
+		MetadataMBeanInfoAssembler assembler = new MetadataMBeanInfoAssembler(attributeSource);
+		MetadataNamingStrategy namingStrategy = new MetadataNamingStrategy(attributeSource);
+		MBeanExporter mBeanExporter = new MBeanExporter();
+		mBeanExporter.setAssembler(assembler);
+		mBeanExporter.setNamingStrategy(namingStrategy);
+		mBeanExporter.setAutodetect(true);
+		return mBeanExporter;
 	}
 }
